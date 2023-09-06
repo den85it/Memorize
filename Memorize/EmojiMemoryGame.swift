98bc82fa7b13615b7e0eaf3bbd8ea1c619f7f1e2
Denis <den85@ya.ru>
2023-09-07 02:48:48 +0500

    Lecture 4 | Stanford CS193p 2023

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -7,7 +7,33 @@
 
 import SwiftUI
 
-class EmojiMemoryGame {
-    var model: MemoryGame<String>
+class EmojiMemoryGame: ObservableObject {
+    private static let emojis = ["👻", "🎃", "👻", "😈", "👻", "🎃", "👻", "😈", "👻", "🎃", "👻", "😈", "👻", "🎃", "👻", "😈"]
     
+    private static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 16) { pairIndex in
+            if emojis.indices.contains(pairIndex) {
+                return emojis[pairIndex]
+            } else {
+                return "💰"
+            }
+            
+        }
+    }
+    
+    @Published private var model = createMemoryGame()
+    
+    //MARK: - Intents
+    
+    func shuffle() {
+        model.shuffle()
+    }
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
+    }
 }
